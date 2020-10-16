834e0b7fe92a5a23fe2242f67e5af1b0f8aa07cb
MauriceNorden <43381188+MauriceNorden@users.noreply.github.com>
2020-10-16 15:47:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,20 +20,19 @@ echo "(____  /____/ |__|  \____/  |__|___|  /____  > |__| (____  /____/____/"
 echo "     \/                             \/     \/            \/           "
 echo ""
 echo ""
-sudo apt --fix-broken install
 sudo apt update && sudo apt upgrade -y
 sudo apt install qemu qemu-kvm libvirt-bin python-paramiko python-pil novnc python-libvirt python-ethtool python-ipaddr python-guestfs libguestfs-tools spice-html5 spice-html5 python-magic keyutils libnfsidmap2 libtirpc1 nfs-common rpcbind python-configobj python-parted -y
 sudo add-apt-repository universe
+kvm-ok
 sudo apt install nginx -y
 sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/wok-2.4.0-0.noarch.deb
 sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/kimchi-2.4.0-0.noarch.deb
 sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/ginger-base-2.3.0-0.noarch.deb
 sudo dpkg -i wok-2.4.0-0.noarch.deb
-sudo dpkg -i ginger-base-2.3.0-0.noarch.deb
+sudo apt install -f -y
+sudo dpkg -i wok-2.3.0-0.noarch.deb
 sudo apt install -f -y
 sudo dpkg --ignore-depends=python-imaging -i kimchi-2.4.0-0.noarch.deb
-sudo apt install -f
-sudo apt --fix-broken install
 sudo ufw disable
 echo ""
 echo ""
