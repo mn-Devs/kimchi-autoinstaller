61181afea914a305ebdeda0de3764f302961ef5b
MauriceNorden <43381188+MauriceNorden@users.noreply.github.com>
2020-10-16 16:31:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,21 +21,20 @@ echo "     \/                             \/     \/            \/           "
 echo ""
 echo "v1"
 echo ""
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install qemu qemu-kvm libvirt-bin python-paramiko python-pil novnc python-libvirt python-ethtool python-ipaddr python-guestfs libguestfs-tools spice-html5 spice-html5 python-magic keyutils libnfsidmap2 libtirpc1 nfs-common rpcbind python-configobj python-parted -y
-sudo add-apt-repository universe
-sudo apt install -f -y
-sudo apt install nginx -y
-sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/wok-2.4.0-0.noarch.deb
-sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/kimchi-2.4.0-0.noarch.deb
-sudo wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/ginger-base-2.3.0-0.noarch.deb
-sudo dpkg -i wok-2.4.0-0.noarch.deb
-sudo apt install -f -y
-sudo dpkg -i wok-2.3.0-0.noarch.deb
-sudo apt install -f -y
-sudo dpkg --ignore-depends=python-imaging -i kimchi-2.4.0-0.noarch.deb
-sudo ufw disable
+apt-get update
+apt-get upgrade
+apt-get install qemu qemu-kvm libvirt-bin python-paramiko python-pil novnc python-libvirt python-ethtool python-ipaddr python-guestfs libguestfs-tools spice-html5 spice-html5 python-magic keyutils libnfsidmap2 libtirpc1 nfs-common rpcbind python-configobj python-parted -y
+apt-get install -f -y
+apt-get install nginx -y
+wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/wok-2.4.0-0.noarch.deb
+wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/kimchi-2.4.0-0.noarch.deb
+wget https://github.com/kimchi-project/kimchi/releases/download/2.4.0/ginger-base-2.3.0-0.noarch.deb
+dpkg -i wok-2.4.0-0.noarch.deb
+apt-get install -f -y
+dpkg -i wok-2.3.0-0.noarch.deb
+apt-get install -f -y
+dpkg --ignore-depends=python-imaging -i kimchi-2.4.0-0.noarch.deb
+ufw disable
 echo ""
 echo ""
 echo "DONE!"
